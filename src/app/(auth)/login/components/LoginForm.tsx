251b23c8db5e3d5450f8ecc8f2bc2f4f4fc3dffd
dxncat <xidarkmoon@gmail.com>
2025-03-20 10:37:54 -0500

    Agregar autenticación con NextAuth, implementar inicio y cierre de sesión, y mejorar el formulario de inicio de sesión

--- src/app/(auth)/login/components/LoginForm.tsx
+++ src/app/(auth)/login/components/LoginForm.tsx
@@ -1,9 +1,26 @@
-import { Key, Mail } from 'lucide-react'
+"use client";
+
+import { CircleAlert, Key, Mail } from 'lucide-react'
+import { authenticate } from '@/actions'
+import clsx from 'clsx'
+import { useRouter } from 'next/navigation'
+import { useActionState, useEffect } from 'react'
+import { useFormStatus } from 'react-dom'
 import React from 'react'
 
 export const LoginForm = () => {
+
+    const [state, dispatch] = useActionState(authenticate, undefined)
+    const router = useRouter()
+
+    useEffect(() => {
+        if (state === "Success") {
+            router.replace('/')
+        }
+    }, [state])
+
     return (
-        <form className='flex flex-col items-center'>
+        <form className='flex flex-col items-center' action={dispatch}>
             <div className='flex items-center bg-gray-100 w-64 p-2 mb-3 rounded-lg'>
                 <Mail className='text-gray-400 mr-2' />
                 <input type="email" name='email' placeholder='Correo' className='text-gray-900 outline-none text-sm flex-1' />
@@ -12,9 +29,33 @@ export const LoginForm = () => {
                 <Key className='text-gray-400 mr-2' />
                 <input type="password" name='password' placeholder='Contraseña' className='text-gray-900 outline-none text-sm flex-1' />
             </div>
-            <button className="border-2 border-white rounded-lg px-12 py-2 inline-block font-semibold mt-16 hover:bg-white hover:text-gray-800 transition-all cursor-pointer">
-                Iniciar Sesión
-            </button>
+
+            {state === "CredentialsSignin" && (
+                <div className="flex flex-row mb-2">
+                    <CircleAlert className="size-5 text-red-500" />
+                    <p className="text-sm text-red-500">Credenciales Invalidas</p>
+                </div>
+            )}
+
+            <LoginButton />
         </form>
+    )
+}
+
+
+function LoginButton() {
+    const { pending } = useFormStatus();
+
+    return (
+        <button
+            type="submit"
+            className={clsx({
+                "btn-login": !pending,
+                "btn-disabled": pending
+            })}
+            disabled={pending}
+        >
+            Iniciar Sesión
+        </button>
     )
 }
---
+++ src/auth.config.ts
@@ -0,0 +1,40 @@
+import NextAuth, { type NextAuthConfig } from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import { z } from "zod";
+import prisma from "./lib/prisma";
+import bcryptjs from "bcryptjs";
+
+export const authConfig: NextAuthConfig = {
+    pages: {
+        signIn: "/login",
+        newUser: "/register",
+    },
+    providers: [
+        Credentials({
+            async authorize(credentials) {
+                const parsedCredentials = z
+                    .object({ email: z.string().email(), password: z.string().min(6) })
+                    .safeParse(credentials)
+
+                if (!parsedCredentials.success) return null
+
+                const { email, password } = parsedCredentials.data
+
+                // buscar usuario en la base de datos
+                const user = await prisma.user.findUnique({ where: { email: email.toLowerCase() } })
+
+                if (!user) return null
+
+                if (!bcryptjs.compareSync(password, user.password)) return null
+
+                //regresar el usuario sin el password
+                const { password: _, ...rest } = user
+
+                return rest
+            }
+        })
+    ]
+}
+
+
+export const { signIn, signOut, auth } = NextAuth(authConfig)
